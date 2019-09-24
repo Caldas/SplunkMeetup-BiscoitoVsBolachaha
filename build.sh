86f7e83743100d34b71f54db5dd5afe96fc37c41
ohmyguigs <ohmyguigs@gmail.com>
2019-09-24 13:05:23 -0300

    Updates env vars

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 rm -rf dist/*
 
-PP_SPLUNK_HOST=http-inputs-parallelpiper.splunkcloud.com \
+PP_SPLUNK_HOST=54.152.83.228 \
 PP_SPLUNK_PORT=8088 \
-PP_SPLUNK_SSL=true \
-PP_SPLUNK_TOKEN=1FD79E2B-019C-4A32-B903-CFFFB7E8ADEA \
+PP_SPLUNK_SSL=false \
+PP_SPLUNK_TOKEN=1d17aa9e-a709-45ab-a4fe-100fe1716960 \
 ./node_modules/webpack/bin/webpack.js -p --progress
