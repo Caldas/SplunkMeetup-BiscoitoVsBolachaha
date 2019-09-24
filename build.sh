882f4497073466febfb1525c44d571a1fa2fceba
ohmyguigs <ohmyguigs@gmail.com>
2019-09-24 13:22:52 -0300

    Updates env vars again 👀

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 rm -rf dist/*
 
-PP_SPLUNK_HOST=https://54.152.83.228 \
+PP_SPLUNK_HOST=54.152.83.228 \
 PP_SPLUNK_PORT=8088 \
-PP_SPLUNK_SSL=false \
+PP_SPLUNK_SSL=true \
 PP_SPLUNK_TOKEN=1d17aa9e-a709-45ab-a4fe-100fe1716960 \
 ./node_modules/webpack/bin/webpack.js -p --progress
