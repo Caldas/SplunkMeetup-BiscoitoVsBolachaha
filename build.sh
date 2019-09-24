bbc5671c258883e6f57b6e9bae5084b699bedfc0
ohmyguigs <ohmyguigs@gmail.com>
2019-09-24 15:06:16 -0300

    Updates env vars for the last time 🚀

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 rm -rf dist/*
 
-PP_SPLUNK_HOST=54.152.83.228 \
+PP_SPLUNK_HOST=splunk-meetup.vtex.com \
 PP_SPLUNK_PORT=8088 \
 PP_SPLUNK_SSL=true \
-PP_SPLUNK_TOKEN=1d17aa9e-a709-45ab-a4fe-100fe1716960 \
+PP_SPLUNK_TOKEN=86bcae3a-b60b-4808-8222-5990ac533b7b \
 ./node_modules/webpack/bin/webpack.js -p --progress
